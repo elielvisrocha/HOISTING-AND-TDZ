c0112d6061402c877cead7db58b4a87175bad208
YashiroYsana <rochaelielvis@gmail.com>
2023-03-18 02:05:48 -0300

    starter

---
+++ script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+
+// function calcAge(birthYear) {
+//     const age = 2029 - birthYear;
+//     console.log(firstName);
+//     return age;
+// }
+
+// const firstName = 'Elvis';
+// calcAge()
+
+// function calcAge(birthYear) {
+//     const age = 2029 - birthYear;
+
+//     function printAge() {
+//         let output = `${firstName} ,you are ${age}, born in ${birthYear}.`;
+//         console.log(output);
+
+//         if (birthYear >= 1981 && birthYear <= 1996) {
+//             const firstName = "Marye";
+//             const str = `Oh, you're a millenial, ${firstName}.`;
+//             console.log(str);
+
+//             function add(a, b) {
+//                 return console.log(a + b)
+//             }
+
+//             const output = "NEW OUTPUT!"
+//         }
+
+//         console.log(output);
+//         // add(2, 3);
+//     }
+//     printAge()
+
+//     return age;
+// }
+
+// const firstName = 'Elvis';
+// calcAge(1990)
+
+
+
+
+
+
+// HOISTING
+
+
